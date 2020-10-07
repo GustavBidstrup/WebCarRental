d66559a93798a8b405a896af969c659efe15efcf
thor <thor@cphbusiness.dk>
2020-10-07 21:56:46 +0200

    third

--- src/main/java/com/wulf/dbaccess/CarMapper.java
+++ src/main/java/com/wulf/dbaccess/CarMapper.java
@@ -7,8 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.wulf.dbaccess.DBConnector;
-import com.wulf.function.Cars;
 import com.wulf.function.entities.Car;
 
 import javax.annotation.Resource;
